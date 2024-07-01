90a8b29e5c11c2f3b4e03dd18a821efd7acd9625
jjkochan-metro <jjkochan@metropolitanind.com>
2024-07-01 13:42:36 -0500

    Update log_test.sh

--- log_test.sh
+++ log_test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "test" >> /var/log/test.log
-echo "$(date) McSquizzy: The Mighty Squirrel" >> /home/SchnippyMcSnapperson/test_folder/test.log
+echo "$(date) Lorraine: The Mighty Companion" >> /home/SchnippyMcSnapperson/test_folder/test.log
 
 sleep 10
 
