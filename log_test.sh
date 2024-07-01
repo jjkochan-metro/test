e267021d891a3333996931a0960c1063a3ddfc17
jjkochan-metro <jjkochan@metropolitanind.com>
2024-07-01 13:23:15 -0500

    Create log_test.sh

---
+++ log_test.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "test" >> /var/log/test.log
+echo "McSquizzy: The Mighty Squirrel" >> /home/SchnippyMcSnapperson/test_folder/test.log
+
+sleep 10
+
+bash /home/SchnippyMcSnapperson/test_folder/log_test.sh
